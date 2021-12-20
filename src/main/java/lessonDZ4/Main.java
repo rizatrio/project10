e1ee3243b8856bde5dafbb8277cb780c7f868c03
rizatrio <90304244+rizatrio@users.noreply.github.com>
2021-12-20 10:08:10 +0600

    Update Main.java
    first fix

--- src/main/java/lessonDZ4/Main.java
+++ src/main/java/lessonDZ4/Main.java
@@ -16,12 +16,19 @@ public class Main {
             return builder.reverse().toString();
         }));
 
-        System.out.println(maximum(l, list -> list.stream().max(Integer::compare).get()));
+        // System.out.println(maximum(l, list -> list.stream().max(Integer::compare).get()));
+        System.out.println(maximum(l, list -> list.stream().max(Integer::compare).orElse(0)));
 
         System.out.println(average(l, list -> list.stream().mapToInt(value -> value).average().orElse(0)));
 
-        List<String> listString = List.of("String", "average", "maximum", "reverse", "Almaty", "Search", "analyze", "interview", "agent");
-        System.out.println(search(listString, list -> list.stream().filter(str -> str.startsWith("a")).collect(Collectors.toList())));
+       // List<String> listString = List.of("String", "average", "maximum", "reverse", "Almaty", "Search", "analyze", "interview", "agent");
+       // System.out.println(search(listString, list -> list.stream().filter(str -> str.startsWith("a")).collect(Collectors.toList())));
+        
+        List<String> listString = List.of("String", "average", "ali", "maximum", "reverse", "Almaty", "Search", "analyze", "interview", "agent", "abc");
+        System.out.println(search(listString, list -> list.stream()
+                .filter(str ->
+                        str.length() == 3 && str.startsWith("a"))
+                .collect(Collectors.toList())));
     }
 
     public static int search(List<Integer> list, Function<List<Integer>, Integer> function) {
